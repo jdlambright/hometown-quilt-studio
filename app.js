cf26ad13d65236a8415e9271e824e8bbc5a235c4
jdlambright <64182772+jdlambright@users.noreply.github.com>
2020-10-25 16:36:08 -0400

    finish app.js template

--- app.js
+++ app.js
@@ -8,8 +8,16 @@ const _ = require("lodash");
 const app= express();
 
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.set("view-engine", "ejs");
+
 
 
 app.get("/", function(req, res){
   req.send("hello");
+});
+
+app.listen(3000, function(){
+  console.log("boy oh boy");
 });
