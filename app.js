e39f65768e171838a81e7d4300168e5e8c747ed9
jdlambright <64182772+jdlambright@users.noreply.github.com>
2020-11-01 16:54:27 -0500

    got ejs set up

--- app.js
+++ app.js
@@ -7,15 +7,15 @@ const _ = require("lodash");
 
 const app= express();
 
+app.set('view-engine', 'ejs');
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
-
-app.set("view-engine", "ejs");
 
 
 
 app.get("/", function(req, res){
-  req.send("hello");
+  res.render("home");
 });
 
 app.listen(3000, function(){
